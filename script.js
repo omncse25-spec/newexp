f435a0627e5ae85af87371fdae84e5787d3ebca0
omncse25-spec <omn.cse25@sbjit.edu.in>
2026-04-27 22:28:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function showPage(pageId) {
     let pages = document.querySelectorAll('.page');
-
-    pages.forEach(function(page) {
-        page.classList.remove('active-page');
-    });
+    pages.forEach(p => p.classList.remove('active-page'));
 
     document.getElementById(pageId).classList.add('active-page');
 }
