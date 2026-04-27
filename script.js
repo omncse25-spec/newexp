26b829d528230fa2f90b1c75c9d4b98bd82ec84f
omncse25-spec <omn.cse25@sbjit.edu.in>
2026-04-27 22:07:43 +0530

    Add rating system to feedback form

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Page Switch
 function showPage(pageId) {
     let pages = document.querySelectorAll('.page');
 
@@ -8,21 +9,42 @@ function showPage(pageId) {
     document.getElementById(pageId).classList.add('active-page');
 }
 
+// ⭐ Rating System
+function setRating(value) {
+    let stars = document.querySelectorAll('.rating span');
+    document.getElementById("ratingValue").value = value;
 
-// 🔥 FEEDBACK FORM JS
+    stars.forEach((star, index) => {
+        if (index < value) {
+            star.classList.add("active");
+        } else {
+            star.classList.remove("active");
+        }
+    });
+}
+
+// Form Submit
 document.addEventListener("DOMContentLoaded", function () {
-
     let form = document.getElementById("feedbackForm");
 
     if (form) {
-        form.addEventListener("submit", function (e) {
+        form.addEventListener("submit", function(e){
             e.preventDefault();
+
+            let rating = document.getElementById("ratingValue").value;
+
+            if (rating === "") {
+                alert("Please select rating ⭐");
+                return;
+            }
 
             document.getElementById("feedbackMsg").innerHTML =
                 "✅ Thank you for your feedback!";
 
             form.reset();
+
+            let stars = document.querySelectorAll('.rating span');
+            stars.forEach(star => star.classList.remove("active"));
         });
     }
-
 });
