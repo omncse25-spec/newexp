6c94b4d86cc1a38b2ef96b6db99006451ec328c9
omncse25-spec <omn.cse25@sbjit.edu.in>
2026-04-28 12:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// PAGE SWITCH FUNCTION
-function showPage(pageId) {
-  document.querySelectorAll('.page').forEach(p => p.classList.remove('active-page'));
-  document.getElementById(pageId).classList.add('active-page');
-
-  // Jab gallery open hogi tab images load hongi
-  if (pageId === "gallery") {
-    loadImages();
-  }
-}
-
-// GALLERY API FUNCTION
-async function loadImages() {
+function loadImages() {
   const galleryContainer = document.querySelector("#gallery .row");
 
-  galleryContainer.innerHTML = "Loading...";
+  galleryContainer.innerHTML = "";
 
-  try {
-    const response = await fetch(
-      "https://api.unsplash.com/photos/random?count=9&client_id=YOUR_ACCESS_KEY"
-    );
-
-    const data = await response.json();
-
-    galleryContainer.innerHTML = "";
-
-    data.forEach(img => {
-      galleryContainer.innerHTML += `
-        <div class="col-md-4">
-          <img src="${img.urls.regular}" class="gallery-img">
-        </div>
-      `;
-    });
-
-  } catch (error) {
-    galleryContainer.innerHTML = "Failed to load images 😢";
-    console.log(error);
+  for (let i = 0; i < 9; i++) {
+    galleryContainer.innerHTML += `
+      <div class="col-md-4">
+        <img src="https://source.unsplash.com/400x300/?food" class="gallery-img">
+      </div>
+    `;
   }
 }
