5f8e83d73a259b125d9ac5222b4699379694531d
omncse25-spec <omn.cse25@sbjit.edu.in>
2026-04-28 14:24:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,3 @@ function loadImages() {
     `;
   }
 }
-function showPage(pageId) {
-  document.querySelectorAll('.page').forEach(p => p.classList.remove('active-page'));
-  document.getElementById(pageId).classList.add('active-page');
-}
