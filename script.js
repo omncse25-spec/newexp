3559989c5702b03bb7324959736b877d16a02376
omncse25-spec <omn.cse25@sbjit.edu.in>
2026-04-30 11:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Page switching function
+
 function showPage(pageId) {
 
-    // sab pages hide karo
+
     let pages = document.querySelectorAll(".page");
     pages.forEach(function(page) {
         page.classList.remove("active-page");
     });
 
-    // selected page show karo
+    
     document.getElementById(pageId).classList.add("active-page");
 }
 
 
-// Default page load hone par HOME show ho
+
 document.addEventListener("DOMContentLoaded", function () {
     showPage("home");
 });
